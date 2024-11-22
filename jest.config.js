241ef0078f2c803ac6a1febc5e76a693928df90a
Gedione Souza <gedionesouza2003@gmail>
2024-11-22 15:05:36 -0300

    first commit

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  textEnvironment: 'jsdom', // Ambiente de teste
+  testPathIgnorePatterns: ['/node_modules', '/.next/'], // Ignorar essas pastas
+  collectCoverage: true, // Cobertura de testes
+  collectCoverageFrom: ['src/**/*.ts(x)?'], // Onde será feita a cobertura de testes
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Configura/carrega esse arquivo antes de tudo que neste caso é o arquivo "setup.ts"
+  modulePaths: ['<rootDir>/src/'], // Pegar todos os arquivos da pasta src
+  transform: {
+    // Configuração do formato que terá os arquivos compilados do Babel
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
+}
